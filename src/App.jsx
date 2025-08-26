4a21c9f049c94687c8f399442fd0589550e18064
hackanonimous <esi.segurity@gmail.com>
2025-08-25 21:22:13 -0500

    fear:crea footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Problem} from './components/Problem/Problem'
 import { Solution } from './components/Solution/Solution'
 import { Technologie } from './components/Technologie/Technologie'
 import { Contact } from './components/Contact/Contact'
+import { Footer } from './components/Footer/Footer'
 function App(){
   return(
     <>
@@ -13,6 +14,7 @@ function App(){
       <Solution/>
       <Technologie/>
       <Contact/>
+      <Footer/>
     </>
   )
 }
