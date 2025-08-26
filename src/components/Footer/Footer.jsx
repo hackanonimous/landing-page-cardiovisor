eebda6c136ffec40bad9bd59124a370ef3613268
hackanonimous <esi.segurity@gmail.com>
2025-08-25 21:37:13 -0500

    fix: corrigue footer
    se aplica los estilos y se termina con el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 export const Footer=()=>{
   return(
-    <section className="bg-(--text-secondary) mt-36 px-10 sm:px-10 md:px-15 lg:px-30 xl:px-36">
-      <div>
-        <h1>CardioVisor</h1>
+    <section className="bg-(--bg-light) mt-36 px-10 sm:px-10 md:px-15 lg:px-30 xl:px-36">
+      <div className="py-6 text-(--text-primary)">
+        <h1 className="text-(--primary) font-bold">CardioVisor</h1>
         <p>Innovación para salud sin barreras.</p>
         <span>© 2025 CardioVisor. Todos los derechos reservados.</span>
       </div>
